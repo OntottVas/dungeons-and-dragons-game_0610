9b4f6e4211887185f58a9abf3ec8d2a227c75fa8
Geree <smithzero94@gmail.com>
2023-06-10 16:24:47 +0200

    Character class defined as comments suggested. Monster class in progress.

--- src/model/Monster.java
+++ src/model/Monster.java
@@ -3,13 +3,37 @@ package model;
 public class Monster {
 
     // Vegyél fel két fieldet: name (String) és health (int)
+    private String name;
+    private int health;
 
     // Írj egy két paraméteres kosntruktort ami beállítja az attribútumok értékeit!
+    public Monster(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
 
     public void attack(Character player) {
         // Írd ki: [szörny neve] attacks [játékos neve]!
+        System.out.println(this.name + " attacks " + player.getName());
 
         // Csökkentsd a játékos életét 20-szal!
+        player.setHealth(player.getHealth() - 20);
 
         // Írd ki: Dealt 20 damage to [játékos neve]!
 
